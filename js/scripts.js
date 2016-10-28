5e785c45bb651c5b94b7a2dd4292471e697e66e4
Brian Pritt <brianpritt@gmail.com>
2016-10-28 11:50:58 -0700

    add basic styling to page, and fix all functionallity issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 //Business
 var isNumber = function(questionalbeInput){//checks to see if input is a number true = is false = not
   return (/\d/.test(questionalbeInput));
-}
-
+}//end isNumber
+var isPositive =function(questionalbeInput){
+  return (questionalbeInput > 0);
+}//end isPositive
 var numberLooper = function(countTo){//loops through range of 1 to userInput, makes an array and replaces some numbers with strings
   var loopArray =[];
 
@@ -16,10 +18,10 @@ var numberLooper = function(countTo){//loops through range of 1 to userInput, ma
       loopArray[index-1] = "ping"
     }else {
       loopArray[index -1] = index ;
-    }
-  }
+    }//end else
+  }//end for loop
   return loopArray;
-}
+}//end numberLooper
 
 //User Interface
 $(document).ready(function(){
@@ -30,16 +32,21 @@ $(document).ready(function(){
     $("#output li").remove(); //clears output section so page doesn't need to be refreshed
     var userInput = parseInt($("#number-input").val());
     $("#number-input").val("");//clears input field
+    $(".warning-label").text("")
+
 
     if (isNumber(userInput) === false){
-      alert("Please enter a number.")
+      $(".warning-label").text("Please enter numbers only.")
+
+    } else if(isPositive(userInput)=== false){
+      $(".warning-label").text("Please enter numbers greater than 0.")
     } else{
      var finalList = numberLooper(userInput);
      for (index = 0; index < userInput; index ++){
-       $("#output ul").append("<li>"+finalList[index]+"</li>");
-     }
-  }
+       $("#output ul").append("<li>"+finalList[index]+" | </li>");
+     }//end for loop
+  }//end else
 
 
-  });
-});
+});//end event listener for submit
+});//end document
